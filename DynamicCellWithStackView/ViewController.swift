d2a1c874e7eb3afde589b3a5b975ea63282dec46
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-06-04 11:02:22 +0800

    Initial Commit

---
+++ DynamicCellWithStackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicCellWithStackView
+//
+//  Created by Chung Han Hsin on 2024/6/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
